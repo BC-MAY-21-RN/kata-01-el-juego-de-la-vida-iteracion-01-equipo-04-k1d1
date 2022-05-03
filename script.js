f71e54afd8fa5ec4cf3c862a7b5b3ebca335ddbe
FranciscoSenestrari <fran.senes@gmail.com>
2022-05-03 18:16:20 -0300

    francisco

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
-var filas;
-var columnas;
 
 class Celula {
-    constructor(estado,x,y){
+    constructor(estado,x,y,limites,cantv){
         this.estado=estado;
         this.x=x;
         this.y=y;
+        this.limites= limites;
+        this.cantv = cantv;
+
     }
+
+    get_estado(){
+        return this.estado
+    }
+    set_estado(nuevo_estado){
+        this.estado = nuevo_estado;
+    }    
+    get_position(){
+        console.log("["+this.x+" , "+this.y+"]");
+    }
+    
+}
+class Table {
+    constructor (filas, columnas)
+    {
+        this.filas = filas ;
+        this.columnas = columnas;
+    }
+    
+
+}
+class Juego{
+
+    constructor(Table)
 }
