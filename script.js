9e60400b15bd076edeca07c9d8a5059e4c94aead
Noel-S <noel.silva.v@gmail.com>
2022-05-03 22:08:10 -0500

    Solucion del ejercicio

--- script.js
+++ script.js
@@ -1,62 +1,104 @@
 
 class Celula {
-    constructor(estado,x,y,limites,cantv){
-        this.estado=estado;
-        this.x=x;
-        this.y=y;
-        this.limites= limites;
-        this.cantv = cantv;
-
+    constructor(estado){
+        this.estado = estado;
     }
     
-    get_estado(){
-
+    get_estado() {
         return this.estado; //Devueleve el estado: 1- Si esta vivo 0-Si esta muerto
     }
-    set_estado(nuevo_estado){
-        this.estado = nuevo_estado;
-    }    
-    get_position(){
-        console.log("["+this.x+" , "+this.y+"]");
-    }
-    get_limites(){
-        return this.limites;
+
+    set_vecinos(vecinos) {
+        this.vecinos = vecinos;
     }
     
-    escuchar_estado(cantVecinos){
-        if (cantVecinos < 2 && this.estado == 1){
+    verificar_estado() {
+        const vecinos_vivos = this.vecinos.filter(celula => celula.get_estado() === 1).length;
+        if (vecinos_vivos < 2 && this.estado === 1){
             this.estado = 0;
-        } else if (cantVecinos > 3 && this.estado == 1){
+        } else if (vecinos_vivos > 3 && this.estado === 1){
             this.estado = 0;
-        } else if ((cantVecinos === 3 || cantVecinos === 2) && this.estado == 1){
+        } else if ((vecinos_vivos === 3 || vecinos_vivos === 2) && this.estado === 1){
             this.estado = 1;
-        } else if (cantVecinos == 3 && this.estado == 0){
+        } else if (vecinos_vivos === 3 && this.estado === 0){
             this.estado = 1;
         }
-
     }
     
 }
+
 class Table {
-    constructor (filas, columnas)
-    {
+    constructor (filas, columnas) {
         this.filas = filas ;
         this.columnas = columnas;
         this.tabla = new Array(filas)
                     .fill(new Array(columnas).fill(0))
-                    .map((filas,i)=>filas.map((columnas,k)=>new Celula(this.generar_random(),i,k,0,0)));
+                    .map(fila => fila.map(_ => new Celula(this.generar_random())));
+        this.tabla.forEach((fila, y) => {
+            fila.forEach((celula, x) => {
+                let vecinos;
+                if (x === 0) {
+                    if (y === 0) {
+                        vecinos = [this.tabla[y][x+1], this.tabla[y+1][x+1], this.tabla[y+1][x]];
+                    }
+                    if (y === this.filas-1) {
+                        vecinos = [this.tabla[y-1][x], this.tabla[y-1][x+1], this.tabla[y][x+1]];
+                    }
+                } else if (x !== this.columnas-1) {
+                    if (y === 0) {
+                        vecinos = [this.tabla[y][x-1], this.tabla[y][x+1], this.tabla[y+1][x-1], this.tabla[y+1][x], this.tabla[y+1][x+1]];
+                    } else if (y === this.filas-1) {
+                        vecinos = [this.tabla[y][x-1], this.tabla[y][x+1], this.tabla[y-1][x-1], this.tabla[y-1][x], this.tabla[y-1][x+1]];
+                    } else {
+                        vecinos = [this.tabla[y-1][x-1], this.tabla[y-1][x], this.tabla[y-1][x+1], this.tabla[y][x-1], this.tabla[y][x+1], this.tabla[y+1][x-1], this.tabla[y+1][x], this.tabla[y+1][x+1]];
+                    }
+                }
 
+                if (y !== 0 && y !== this.filas-1) {
+                    if (x === 0) {
+                        vecinos = [this.tabla[y-1][x], this.tabla[y-1][x+1], this.tabla[y][x+1], this.tabla[y+1][x+1], this.tabla[y+1][x]];
+                    } else if (x === this.columnas-1) {
+                        vecinos = [this.tabla[y-1][x], this.tabla[y-1][x-1], this.tabla[y][x-1], this.tabla[y+1][x-1], this.tabla[y+1][x]];
+                    } else {
+                        vecinos = [this.tabla[y-1][x-1], this.tabla[y-1][x], this.tabla[y-1][x+1], this.tabla[y][x-1], this.tabla[y][x+1], this.tabla[y+1][x-1], this.tabla[y+1][x], this.tabla[y+1][x+1]];
+                    }
+                }
+
+                if (x === this.columnas-1) {
+                    if (y === 0) {
+                        vecinos = [this.tabla[y][x-1], this.tabla[y+1][x-1], this.tabla[y+1][x]];
+                    }
+                    
+                    if (y === this.filas-1) {
+                        vecinos = [this.tabla[y][x-1], this.tabla[y-1][x-1], this.tabla[y-1][x]];
+                    }
+                }
+
+                celula.set_vecinos(vecinos)
+            });
+        });
     }
+
     generar_random(){
-        let numRandom= Math.round(Math.random())
+        let numRandom = Math.round(Math.random())
         return numRandom;
     }
-    mostrar_tabla (){
-        this.tabla.forEach(fila => console.log(fila.map(columna => columna.get_estado() ? '*' : '.').join('')));
+
+    mostrar_tabla () {
+        console.log(this.tabla.map(fila => fila.map(columna => columna.get_estado() ? '*' : '.').join('').concat('\n')).join(''));
+    }
+
+    siguiente_generacion() {
+        this.tabla.forEach(fila => fila.forEach(celula => celula.verificar_estado()));
     }
 
 }
 
 
-const tabla = new Table(4,4);
-tabla.mostrar_tabla()
+const tabla = new Table(6,10);
+// tabla.mostrar_tabla();
+setInterval(() => {
+    console.clear();
+    tabla.mostrar_tabla();
+    tabla.siguiente_generacion();
+}, 700);
